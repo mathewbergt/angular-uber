fe5887d45798158cb1783bac6f9808a634dde546
Mathew Bergt <mathew.bergt@gmail.com>
2014-09-17 00:41:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -33,31 +33,36 @@ factory('mbUber', function($q, $http) {
     }
     
     function getProducts(latitude, longitude) {
-      latitude = '37.775818';
-      longitude = '-122.418028';
-      serverToken = 'VzlZPiK15pf1rLotl1sJV9rullyxS4Zc-7gK6ILB';
-      var path = 'products';
+      // latitude = '37.775818';
+      // longitude = '-122.418028';
+      // serverToken = 'VzlZPiK15pf1rLotl1sJV9rullyxS4Zc-7gK6ILB';
+      // var path = 'products';
       
-      $http({
-          method: 'GET',
-          url: baseUrl + version + path,
-          params: {
-            latitude: latitude,
-            longitude: longitude,
-            server_token: serverToken
-          },
-          headers: {
-            'Authorization': serverToken
-          }
-      }).
-      success(function(data, status, headers, config) {
-        console.log('success');
-        console.log(data);
-      }).
-      error(function(data, status, headers, config) {
-        console.log('error');
-        console.log(data);
-      });
+      // $http({
+      //     method: 'GET',
+      //     url: baseUrl + version + path,
+      //     params: {
+      //       latitude: latitude,
+      //       longitude: longitude,
+      //       server_token: serverToken
+      //     },
+      //     headers: {
+      //       'Authorization': serverToken
+      //     }
+      // }).
+      // success(function(data, status, headers, config) {
+      //   console.log('success');
+      //   console.log(data);
+      // }).
+      // error(function(data, status, headers, config) {
+      //   console.log('error');
+      //   console.log(data);
+      // });
+      
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823&server_token=VzlZPiK15pf1rLotl1sJV9rullyxS4Zc-7gK6ILB', true);
+      xhr.setRequestHeader("Authorization", "VzlZPiK15pf1rLotl1sJV9rullyxS4Zc-7gK6ILB");
+      xhr.send();
       
       return {};
       
