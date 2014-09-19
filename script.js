a25e071f7d36b010eceb7ff0c259489cad517eff
Mathew Bergt <mathew.bergt@gmail.com>
2014-09-19 08:39:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -60,14 +60,14 @@ factory('mbUber', function($q, $http) {
       //   console.log(data);
       // });
       
-      function myData([{
+      myData([{
         client_secret: 'TrYjG6u07PKGKQt5Y4mM5qjPOee07fj4J9MBS3lb',
         client_id: 'jWWyetwuXd4D8ePGYawyYXRMdj-5MzoL',
         grant_type: 'authorization_code',
         redirect_uri: 'https://mathewbergt.github.io/angular-uber&code=8dk6uFp5pV6KG5gUS6hnhXKqPngtC7'
       }]);
       
-      $http.jsonp('https://login.uber.com/oauth/token?cb=myData')
+      $http.jsonp('https://login.uber.com/oauth/token?myData')
       .success(function(data){
          console.log('success');
       })
