6e5f01beef8b77c648f4b2668facd2e2ed2a84e3
Mathew Bergt <mathew.bergt@gmail.com>
2014-12-17 00:20:11 -0900

    Added a signIn method just for fun.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,10 +7,9 @@
  * # MainCtrl
  * Controller of the angularUberApp
  */
-angular.module('angularUberApp')
-.controller('MainCtrl', function ($scope, myUber) {
-	myUber.setServerToken('VzlZPiK15pf1rLotl1sJV9rullyxS4Zc-7gK6ILB');
-	//console.log(myUber.getServerToken());
-
-	$scope.getProducts = myUber.getProducts;
+angular.module('angularUberApp').
+controller('MainCtrl', function ($scope, myUber) {
+    //myUber.setServerToken('VzlZPiK15pf1rLotl1sJV9rullyxS4Zc-7gK6ILB');
+    $scope.getProducts = myUber.getProducts;
+    $scope.signIn = myUber.signIn;
 });
