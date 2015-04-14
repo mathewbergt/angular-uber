588fee614311c36fd9c6279b37221823a4623cd8
Mathew Bergt <mathew.bergt@gmail.com>
2015-04-14 08:26:25 -0800

    Update angular-uber.js

--- angular-uber.js
+++ angular-uber.js
@@ -1,25 +1,54 @@
-'use strict';
+(function() {
+    'use strict';
 
-/**
- * @ngdoc overview
- * @name angularUberApp
- * @description
- * # angularUberApp
- *
- * Main module of the application.
- */
-angular.module('mbergt.uber', [])
-    .factory('mbUber', function ($q, $http) {
-        return function modalFactory(config) {
+    angular
+        .module('mbergt.uber', [])
+        .factory('mbUber', mbUber);
+
+    mbUber.$inject = ['$q', '$http'];
+
+    function mbUber($q, $http) {
+        return function uberFactory(config) {
             config = config || {};
 
             var baseUrl = config.baseUrl || 'https://api.uber.com',
                 version = config.version || '/v1/',
                 serverToken = config.serverToken,
                 bearerToken = config.bearerToken,
+                useBearerToken = config.useBearerToken,
                 clientId = config.clientId,
                 sandbox = config.sandbox || false,
                 sandboxBaseUrl = config.sandboxBaseUrl || 'https://sandbox-api.uber.com';
+
+            return {
+                setBaseUrl: setBaseUrl,
+                getBaseUrl: getBaseUrl,
+                setVersion: setVersion,
+                getVersion: getVersion,
+                setServerToken: setServerToken,
+                getServerToken: getServerToken,
+                setBearerToken: setBearerToken,
+                getBearerToken: getBearerToken,
+                setUseBearerToken: setUseBearerToken,
+                getUseBearerToken: getUseBearerToken,
+                setSandbox: setSandbox,
+                getSandbox: getSandbox,
+                setSandboxBaseUrl: setSandboxBaseUrl,
+                getSandboxBaseUrl: getSandboxBaseUrl,
+                setClientId: setClientId,
+                getClientId: getClientId,
+                getProducts: getProducts,
+                getProduct: getProduct,
+                getPriceEstimates: getPriceEstimates,
+                getTimeEstimates: getTimeEstimates,
+                getPromotions: getPromotions,
+                getHistory: getHistory,
+                getUserProfile: getUserProfile,
+                makeRequest: makeRequest,
+                getRequestDetails: getRequestDetails,
+                cancelRequest: cancelRequest,
+                getRequestMap: getRequestMap
+            };
 
             function setBaseUrl() {
                 baseUrl = arguments[0];
@@ -53,6 +82,14 @@ angular.module('mbergt.uber', [])
                 return bearerToken;
             }
 
+            function setUseBearerToken() {
+                useBearerToken = arguments[0];
+            }
+
+            function getUseBearerToken() {
+                return useBearerToken;
+            }
+
             function setClientId() {
                 clientId = arguments[0];
             }
@@ -77,10 +114,155 @@ angular.module('mbergt.uber', [])
                 return sandboxBaseUrl;
             }
 
-            function api(endpoint, params, options, apiConfig) {
-                apiConfig = apiConfig || {},
+            function getProducts(latitude, longitude, options) {
+                var apiConfig = buildApiConfig(options);
+
+                return api('products', {
+                    'latitude': latitude,
+                    'longitude': longitude
+                }, apiConfig);
+            }
+
+            function getProduct(productId, options) {
+                var apiConfig = buildApiConfig(options);
+
+                return api('products/' + productId, apiConfig);
+            }
+
+            function getPriceEstimates(startLatitude, startLongitude, endLatitude, endLongitude, options) {
+                var apiConfig = buildApiConfig(options);
+
+                return api('estimates/price', {
+                    'start_latitude': startLatitude,
+                    'start_longitude': startLongitude,
+                    'end_latitude': endLatitude,
+                    'end_longitude': endLongitude
+                }, apiConfig);
+            }
+
+            function getTimeEstimates(startLatitude, startLongitude, customerUuid, productId, options) {
+                var apiConfig = buildApiConfig(options);
+
+                return api('estimates/time', {
+                    'start_latitude': startLatitude,
+                    'start_longitude': startLongitude,
+                    'customer_uuid': customerUuid,
+                    'product_id': productId
+                }, apiConfig);
+            }
+
+            function getPromotions(startLatitude, startLongitude, endLatitude, endLongitude, options) {
+                var apiConfig = buildApiConfig(options);
+
+                return api('promotions', {
+                    'start_latitude': startLatitude,
+                    'start_longitude': startLongitude,
+                    'end_latitude': endLatitude,
+                    'end_longitude': endLongitude
+                }, apiConfig);
+            }
+
+            function getHistory(offset, limit, options) {
+                var apiConfig;
+
                 options = options || {};
 
+                bearerIsDefinedCheck(options, 'history');
+
+                apiConfig = {
+                    'version': options.version || '/v1.1/',
+                    'headers': buildBearerHeaders(options)
+                };
+
+                return api('history', {
+                    'offset': offset,
+                    'limit': limit
+                }, apiConfig);
+            }
+
+            function getUserProfile(options) {
+                var apiConfig;
+
+                options = options || {};
+
+                bearerIsDefinedCheck(options, 'me');
+
+                apiConfig = { 'headers': buildBearerHeaders(options) };
+
+                return api('me', null, apiConfig);
+            }
+
+            function makeRequest(productId, startLatitude, startLongitude, endLatitude, endLongitude, surgeConfirmationId, options) {
+                var apiConfig;
+
+                options = options || {};
+
+                bearerIsDefinedCheck(options, 'requests');
+
+                apiConfig = {
+                    'data': {
+                        'product_id': productId,
+                        'start_latitude': startLatitude,
+                        'start_longitude': startLongitude,
+                        'end_latitude': endLatitude,
+                        'end_longitude': endLongitude,
+                        'surge_confirmation_id': surgeConfirmationId
+                    },
+                    'headers': buildBearerHeaders(options),
+                    'method': 'POST'
+                };
+
+                return api('requests', null, apiConfig);
+            }
+
+            function getRequestDetails(requestId, options) {
+                var apiConfig;
+
+                options = options || {};
+
+                bearerIsDefinedCheck(options, 'requests/{request_id}');
+
+                apiConfig = { 'headers': buildBearerHeaders(options) };
+
+                return api('requests/' + requestId, null, apiConfig);
+            }
+
+            function cancelRequest(requestId, options) {
+                var apiConfig;
+
+                options = options || {};
+
+                bearerIsDefinedCheck(options, 'requests');
+
+                apiConfig = {
+                    'headers': buildBearerHeaders(options),
+                    'method': 'DELETE'
+                };
+
+                return api('requests/' + requestId, null, apiConfig);
+            }
+
+            function getRequestMap(requestId, options) {
+                var apiConfig;
+
+                options = options || {};
+
+                bearerIsDefinedCheck(options, 'requests/{request_id}/map');
+
+                apiConfig = { 'headers': buildBearerHeaders(options) };
+
+                return api('requests/' + requestId + '/map', null, apiConfig);
+            }
+
+            // private methods //
+
+            function bearerIsDefinedCheck(options, endpoint) {
+                if (!options.bearerToken && !bearerToken) {
+                    throw new Error('Bearer token must be specified for ' + endpoint + ' endpoint.');
+                }
+            }
+
+            function apiConfigurationCheck(apiConfig) {
                 if (!endpoint || endpoint.length === 0) {
                     throw new Error('An endpoint must be specified.');
                 }
@@ -92,208 +274,61 @@ angular.module('mbergt.uber', [])
                 if (apiConfig.headers && (!apiConfig.headers.Authorization || apiConfig.headers.Authorization.length === 0)) {
                     throw new Error('The authorization header must be specified for all endpoints.');
                 }
+            }
 
-                function buildBaseUrl() {
-                    var base = (options.sandbox || getSandbox()) ? (options.sandboxBaseUrl || getSandboxBaseUrl()) : (options.baseUrl || getbaseUrl());
-                        base += (options.version || apiConfig.version || version);
-                        //base += (apiConfig.sandbox || sandbox) ? 'sandbox/' : '';
-                        base += endpoint;
-                    return base;
-                }
+            function api(endpoint, params, apiConfig) {
+                var deferred;
 
-                var deferred = $q.defer();
+                apiConfig = apiConfig || {};
 
-                $http({
-                    url: buildBaseUrl(),
-                    method: apiConfig.method || 'GET',
-                    params: params,
-                    data: apiConfig.data,
-                    headers: apiConfig.headers || { Authorization: 'Token ' + (options.serverToken || getServerToken()) }
-                })
-                .success(function (data) {
-                    deferred.resolve(data);
-                })
-                .error(function (data) {
-                    deferred.reject(data);
-                });
+                apiConfigurationCheck(apiConfig);
+
+                deferred = $q.defer();
+
+                $http(apiConfig)
+                    .success(function (data) {
+                        deferred.resolve(data);
+                    })
+                    .error(function (data) {
+                        deferred.reject(data);
+                    });
 
                 return deferred.promise;
             }
 
-            function getProducts(latitude, longitude, options) {
-                return api('products', {
-                    'latitude': latitude,
-                    'longitude': longitude
-                }, options);
-            }
+            function buildApiConfig(options) {
+                var apiConfig;
 
-            function getProduct(productId, options) {
-                return api('products/' + productId, options);
-            }
-
-            function getPriceEstimates(startLatitude, startLongitude, endLatitude, endLongitude, options) {
-                return api('estimates/price', {
-                    'start_latitude': startLatitude,
-                    'start_longitude': startLongitude,
-                    'end_latitude': endLatitude,
-                    'end_longitude': endLongitude
-                }, options);
-            }
-
-            function getTimeEstimates(startLatitude, startLongitude, customerUuid, productId, options) {
-                return api('estimates/time', {
-                    'start_latitude': startLatitude,
-                    'start_longitude': startLongitude,
-                    'customer_uuid': customerUuid,
-                    'product_id': productId
-                }, options);
-            }
-
-            function getPromotions(startLatitude, startLongitude, endLatitude, endLongitude, options) {
-                return api('promotions', {
-                    'start_latitude': startLatitude,
-                    'start_longitude': startLongitude,
-                    'end_latitude': endLatitude,
-                    'end_longitude': endLongitude
-                }, options);
-            }
-
-            function getHistory(offset, limit, options) {
-                options = options || {};
-
-                if (!options.bearerToken && !getBearerToken()) {
-                    throw new Error('Bearer token must be specified for history endpoint.');
+                if (!options.useBearerToken && !useBearerToken) {
+                    apiConfig.headers = { 
+                        'Authorization': 'Token ' + (options.serverToken || apiConfig.serverToken || serverToken) 
+                    };
                 }
 
-                var apiConfig = {};
-                apiConfig.version = options.version || '/v1.1/';
-
-                apiConfig.headers = {
-                    'Authorization': 'Bearer ' + (options.bearerToken || getBearerToken())
+                apiConfig = {
+                    'url': buildUrl(endpoint, apiConfig),
+                    'method': apiConfig.method || 'GET',
+                    'params': params,
+                    'data': apiConfig.data,
+                    'headers': apiConfig.headers || { 'Authorization': 'Token ' + (apiConfig.serverToken || serverToken) }
                 };
 
-                return api('history', {
-                    'offset': offset,
-                    'limit': limit
-                }, options, apiConfig);
             }
 
-            function getUserProfile(options) {
-                options = options || {};
-
-                if (!options.bearerToken && !getBearerToken()) {
-                    throw new Error('Bearer token must be specified for me endpoint.');
-                }
-
-                var apiConfig = {};
-                apiConfig.headers = {
-                    'Authorization': 'Bearer ' + (options.bearerToken || getBearerToken())
-                };
-
-                return api('me', null, options, apiConfig);
+            function buildBearerHeaders(options) {
+                return { 'Authorization': 'Bearer ' + (options.bearerToken || bearerToken) };
             }
 
-            function makeRequest(productId, startLatitude, startLongitude, endLatitude, endLongitude, surgeConfirmationId, options) {
-                options = options || {};
-
-                if (!options.bearerToken && !getBearerToken()) {
-                    throw new Error('Bearer token must be specified for requests endpoint.');
-                }
-
-                var apiConfig = {};
-                apiConfig.data = {
-                    'product_id': productId,
-                    'start_latitude': startLatitude,
-                    'start_longitude': startLongitude,
-                    'end_latitude': endLatitude,
-                    'end_longitude': endLongitude,
-                    'surge_confirmation_id': surgeConfirmationId
-                }
-                console.log(getBearerToken())
-                apiConfig.headers = {
-                    'Authorization': 'Bearer ' + (options.bearerToken || getBearerToken())
-                };
-
-                apiConfig.method = 'POST';
-
-                return api('requests', null, options, apiConfig);
+            function buildUrl(endpoint, apiConfig) {
+                var base;
+                    base = (apiConfig.sandbox || getSandbox()) ?
+                                (apiConfig.sandboxBaseUrl || sandboxBaseUrl) :
+                                (apiConfig.baseUrl || baseUrl);
+                    base += (apiConfig.version || version);
+                    base += endpoint;
+                return base;
             }
+        }
+    }
 
-            function getRequestDetails(requestId, options) {
-                options = options || {};
-
-                if (!options.bearerToken && !getBearerToken()) {
-                    throw new Error('Bearer token must be specified for requests/{request_id} endpoint.');
-                }
-
-                var apiConfig = {};
-                apiConfig.headers = {
-                    'Authorization': 'Bearer ' + (options.bearerToken || getBearerToken())
-                };
-
-                return api('requests/' + requestId, null, options, apiConfig);
-            }
-
-            function cancelRequest(requestId, options) {
-                options = options || {};
-
-                if (!options.bearerToken && !getBearerToken()) {
-                    throw new Error('Bearer token must be specified for requests endpoint.');
-                }
-
-                var apiConfig = {};
-                apiConfig.headers = {
-                    'Authorization': 'Bearer ' + (options.bearerToken || getBearerToken())
-                };
-
-                apiConfig.method = 'DELETE';
-
-                return api('requests/' + requestId, null, options, apiConfig);
-            }
-
-            function getRequestMap(requestId, options) {
-                options = options || {};
-
-                if (!options.bearerToken && !getBearerToken()) {
-                    throw new Error('Bearer token must be specified for requests/{request_id}/map endpoint.');
-                }
-
-                var apiConfig = {};
-                apiConfig.headers = {
-                    'Authorization': 'Bearer ' + (options.bearerToken || getBearerToken())
-                };
-
-                return api('requests/' + requestId + '/map', null, options, apiConfig);
-            }
-
-            return {
-                setBaseUrl: setBaseUrl,
-                getBaseUrl: getBaseUrl,
-                setVersion: setVersion,
-                getVersion: getVersion,
-                setServerToken: setServerToken,
-                getServerToken: getServerToken,
-                setBearerToken: setBearerToken,
-                getBearerToken: getBearerToken,
-                setSandbox: setSandbox,
-                getSandbox: getSandbox,
-                setSandboxBaseUrl: setSandboxBaseUrl,
-                getSandboxBaseUrl: getSandboxBaseUrl,
-                setClientId: setClientId,
-                getClientId: getClientId,
-                getProducts: getProducts,
-                getProduct: getProduct,
-                getPriceEstimates: getPriceEstimates,
-                getTimeEstimates: getTimeEstimates,
-                getPromotions: getPromotions,
-                getHistory: getHistory,
-                getUserProfile: getUserProfile,
-                makeRequest: makeRequest,
-                getRequestDetails: getRequestDetails,
-                cancelRequest: cancelRequest,
-                getRequestMap: getRequestMap
-            };
-
-
-        };
-    });
+})();
